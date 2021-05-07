d49f7ef1a5354a2b64aa39ac20889092e0eef74f
artemio <artemiosotomayor@gmail.com>
2021-05-07 17:51:29 -0500

    Comentarios mi primera funcion

--- MiPrimerFuncion.cpp
+++ MiPrimerFuncion.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
-void saludar(char nombre[]);
+
+void saludar(char nombre[]); // Definicion, formada por tres partes, tipo de dato, nombre y parametros
 
 int main(int argc, char const *argv[])
 {
-    char nombre[254];
+    char nombre[254]; // un arreglo de 254 caracteres
     cout<<"Dime tu nombre: ";
-    cin.getline(nombre,sizeof(nombre));
+    // cin.getline(nombre,254);// Guardo una serie de caracteres utilizando el nombre de la variable y su tamaño
+    cin.getline(nombre,sizeof(nombre)); // BUENA PRACTICA, utilizar la funcion sizeof()
     // cout<<"Hola "<<nombre<<endl;
-    saludar(nombre);
+    saludar(nombre); //llamado o invocacion la funcion con un valor 
     return 0;
 }
 
-void saludar(char nombre[]){
+void saludar(char nombre[]){ // desarrollo de la funcion
     cout<<"Hola "<<nombre<<endl;
 }
 
