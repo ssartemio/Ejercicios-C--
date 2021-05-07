35b078442df5ab0333b8eb70d061cc3c53ba7acc
artemio <artemiosotomayor@gmail.com>
2021-05-07 16:30:00 -0500

    Programa con letras

---
+++ AbecedarioInverso.cpp
@@ -0,0 +1,15 @@
+/*
+Escribe un programa que muestre las letras del abecedario en orden inverso.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        cout << c << " ";
+    }
+    return 0;
+}
